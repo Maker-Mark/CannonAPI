6b62a1b34916e72cbc095f2286d15ebfa94317a8
Mark <Mark@Moshes-MBP.home>
2019-06-19 20:06:17 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log('Hello');
+var current = 182;
+var outside;
+
+
+
+
+function fetchWrapper(url, options, timeout) {
+  return new Promise((res, rej) => {
+    fetch(url, options)
+      .then(response => response.blob())
+      .then(images => {
+        outside = images;
+        console.log('Hello');
+        // console.log(images.type);
+        document.getElementById('main') = outside;
+        current++;
+      })
+    if (timeout) {
+      const e = new Error("Connection timed out")
+      setTimeout(rej, timeout, e)
+    }
+  })
+}
+
+fetchWrapper(`https://api.github.com/users?`, {}, 10000);
