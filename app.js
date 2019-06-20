16e35465883bf2aacf23e549c8c9fe308c784e69
Mark <Mark@Moshes-MBP.home>
2019-06-19 20:28:08 -0400

    fetchwrapper not working

--- app.js
+++ app.js
@@ -3,24 +3,34 @@ var current = 182;
 var outside;
 
 
-
-
-function fetchWrapper(url, options, timeout) {
-  return new Promise((res, rej) => {
-    fetch(url, options)
-      .then(response => response.blob())
-      .then(images => {
-        outside = images;
-        console.log('Hello');
-        // console.log(images.type);
-        document.getElementById('main') = outside;
-        current++;
-      })
-    if (timeout) {
-      const e = new Error("Connection timed out")
-      setTimeout(rej, timeout, e)
-    }
+fetch(`http://192.168.1.7:8080/ccapi/ver100/contents/sd/100CANON/IMG_0183.JPG`)
+  .then(function (response) {
+    return response.blob();
   })
-}
+  .then(images => {
+    outside = URL.createObjectURL(images);
+    console.log('Hello');
+    // console.log(images.type);
+    document.getElementById('main').src = outside;
+    current++;
+  });
 
-fetchWrapper(`https://api.github.com/users?`, {}, 10000);
+// function fetchWrapper(url, options, timeout) {
+//   return new Promise((res, rej) => {
+//     fetch(url, options)
+//       .then(response => response.blob())
+//       .then(images => {
+//         outside = images;
+//         console.log('Hello');
+//         // console.log(images.type);
+//         document.getElementById('main') = outside;
+//         current++;
+//       })
+//     if (timeout) {
+//       const e = new Error("Connection timed out")
+//       setTimeout(rej, timeout, e)
+//     }
+//   });
+// }
+
+// fetchWrapper(`http://192.168.1.7:8080/ccapi/ver100/contents/sd/100CANON/IMG_0181.JPG`, {}, 10000);
