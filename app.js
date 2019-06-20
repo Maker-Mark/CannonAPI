58afadf56c6629382d10cb4ac6ac6f221173bca1
Mark Goldstein <markgoldstein12@gmail.com>
2019-06-19 20:34:43 -0400

    not working

--- app.js
+++ app.js
@@ -5,46 +5,39 @@ var outside;
 // test
 
 
+
+
+
+
+// fetch(`http://192.168.1.7:8080/ccapi/ver100/contents/sd/100CANON/IMG_0182.JPG`)
+//   .then(function (response) {
+//     return response.blob();
+//   })
+//   .then(images => {
+//     outside = URL.createObjectURL(images);
+//     console.log('Hello');
+//     // console.log(images.type);
+//     document.getElementById('main').src = outside;
+//     current++;
+//   });
+
+
 function fetchWrapper(url, options, timeout) {
   return new Promise((res, rej) => {
-      fetch(url, options)
-        .then(response => response.blob())
-        .then(images => {
-          outside = images;
-          console.log('Hello');
-          // console.log(images.type);
-          document.getElementById('main') = outside;
-          current++;
-        })
-      if (timeout) {
-        const e = new Error("Connection timed out")
-        setTimeout(rej, timeout, e)
-      }
-    })
-    .then(images => {
-      outside = URL.createObjectURL(images);
-      console.log('Hello');
-      // console.log(images.type);
-      document.getElementById('main').src = outside;
-      current++;
-    });
+    fetch(url, options)
+      .then(response => response.blob())
+      .then(images => {
+        outside = images;
+        console.log('Hello');
+        // console.log(images.type);
+        document.getElementById('main') = outside;
+        current++;
+      })
+    if (timeout) {
+      const e = new Error("Connection timed out")
+      setTimeout(rej, timeout, e)
+    }
+  });
+}
 
-  // function fetchWrapper(url, options, timeout) {
-  //   return new Promise((res, rej) => {
-  //     fetch(url, options)
-  //       .then(response => response.blob())
-  //       .then(images => {
-  //         outside = images;
-  //         console.log('Hello');
-  //         // console.log(images.type);
-  //         document.getElementById('main') = outside;
-  //         current++;
-  //       })
-  //     if (timeout) {
-  //       const e = new Error("Connection timed out")
-  //       setTimeout(rej, timeout, e)
-  //     }
-  //   });
-  // }
-
-  // fetchWrapper(`http://192.168.1.7:8080/ccapi/ver100/contents/sd/100CANON/IMG_0181.JPG`, {}, 10000);
+fetchWrapper(`http://192.168.1.7:8080/ccapi/ver100/contents/sd/100CANON/IMG_0181.JPG`, {}, 10000);
